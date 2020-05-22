26f5e1efbed2ddeda12151f0cdc20ecbdb7f526a
stefaleon <stefaleon@gmail.com>
2020-05-22 23:54:40 +0300

    Loading a Window File & Adding an Icon

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ function createMainWindow() {
     title: 'ImageShrink',
     width: 500,
     height: 600,
+    icon: './assets/icons/Icon_256x256.png',
   });
+
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile('./app/index.html');
 }
 
 app.on('ready', createMainWindow);
