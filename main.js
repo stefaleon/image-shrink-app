478418fe800d8602cb6ccd7a492d2a52ebfa94e4
stefaleon <stefaleon@gmail.com>
2020-05-22 23:30:48 +0300

    Setup & BrowserWindow

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    width: 500,
+    height: 600,
+  });
+}
+
+app.on('ready', createMainWindow);
