ee5c49b0ef35c9f89b18272f0d0fdab7603353e2
stefaleon <stefaleon@gmail.com>
2020-05-23 00:17:34 +0300

    Environment & Platform Check

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const { app, BrowserWindow } = require('electron');
 
+// Set env
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+const isWin32 = process.platform === 'win32' ? true : false;
+
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
     height: 600,
     icon: './assets/icons/Icon_256x256.png',
+    resizable: isDev ? true : false,
   });
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
@@ -13,3 +21,19 @@ function createMainWindow() {
 }
 
 app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
