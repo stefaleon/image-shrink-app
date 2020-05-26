c2144807949692e1624498ff1ab4bdfce42e11ac
stefaleon <stefaleon@gmail.com>
2020-05-27 01:47:51 +0300

    Implementing Imagemin & Plugins

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
 
 // Set env
 process.env.NODE_ENV = 'development';
@@ -99,8 +105,29 @@ const menu = [
 ];
 
 ipcMain.on('image:minimize', (e, options) => {
-  console.log(options);
+  options.dest = path.join(os.homedir(), 'imageshrink');
+  shrinkImage(options);
 });
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({ quality: [pngQuality, pngQuality] }),
+      ],
+    });
+
+    console.log(files);
+
+    shell.openPath(dest);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
