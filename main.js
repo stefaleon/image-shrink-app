4daf7d7fc2f18dd8b777e3f7714cc29940539c14
stefaleon <stefaleon@gmail.com>
2020-05-26 15:00:53 +0300

    Renderer Node Integration

--- main.js
+++ main.js
@@ -13,12 +13,19 @@ let aboutWindow;
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: 'ImageShrink',
-    width: 500,
+    width: isDev ? 800 : 500,
     height: 600,
     icon: './assets/icons/Icon_256x256.png',
     resizable: isDev ? true : false,
     backgroundColor: 'teal',
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
   mainWindow.loadFile('./app/index.html');
